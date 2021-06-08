c39a7e28fe2dbe03af3a30b95e2199e8498ba23b
drakumus <rohancheeniyil@gmail.com>
2021-06-08 17:11:06 -0600

    Add release workflow and todo

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ output "ssh_address" {
 }
 
 resource "local_file" "ssh_session_file" {
-
+  # TODO use variable to pass file location of private key here
   content = "terraform = #109#0%${digitalocean_droplet.web.ipv4_address}%22%root%%-1%-1%%%22%%0%0%0%_ProfileDir_\\Documents\\rsa\\terraform\\project_server%%-1%0%0%0%%1080%%0%0%1#MobaFont%10%0%0%0%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%-1%_Std_Colors_0_%80%24%0%1%-1%<none>%%0#0# #-1"
   filename = "${path.module}/terraform.moba"
 }
