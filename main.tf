a9ceef10801867f5d5a7ff22813a74fca723da42
drakumus <rohancheeniyil@gmail.com>
2021-06-08 17:15:30 -0600

    lint main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
@@ -19,30 +19,30 @@ resource "digitalocean_ssh_key" "default" {
 
 # need variables do_token, private_key for this to work
 resource "digitalocean_droplet" "web" {
-  image  = "docker-20-04"
-  name   = "ProjectServer1"
-  region = "nyc3"
-  size   = "s-1vcpu-1gb"
+  image    = "docker-20-04"
+  name     = "ProjectServer1"
+  region   = "nyc3"
+  size     = "s-1vcpu-1gb"
   ssh_keys = [digitalocean_ssh_key.default.fingerprint]
 
-# working code to connect and execute commands on remote after creation
+  # working code to connect and execute commands on remote after creation
   # connection exists for provisioner to connect
-#  connection {
-#    host = self.ipv4_address
-#    user = "root"
-#    type = "ssh"
-#    private_key = file(var.private_ssh_key)
-#    timeout = "10m"
-#  }
-#  provisioner "remote-exec" {
-#    inline = [
-#      #"export PATH=$PATH:/usr/bin",
-#      #install nginx
-#      #"sudo apt-get update",
-#      #"sudo apt-get -y install nginx"
-#      "ip address show"
-#    ]
-#  }
+  #  connection {
+  #    host = self.ipv4_address
+  #    user = "root"
+  #    type = "ssh"
+  #    private_key = file(var.private_ssh_key)
+  #    timeout = "10m"
+  #  }
+  #  provisioner "remote-exec" {
+  #    inline = [
+  #      #"export PATH=$PATH:/usr/bin",
+  #      #install nginx
+  #      #"sudo apt-get update",
+  #      #"sudo apt-get -y install nginx"
+  #      "ip address show"
+  #    ]
+  #  }
 }
 
 output "ssh_address" {
@@ -51,7 +51,7 @@ output "ssh_address" {
 
 resource "local_file" "ssh_session_file" {
   # TODO use variable to pass file location of private key here
-  content = "terraform = #109#0%${digitalocean_droplet.web.ipv4_address}%22%root%%-1%-1%%%22%%0%0%0%_ProfileDir_\\Documents\\rsa\\terraform\\project_server%%-1%0%0%0%%1080%%0%0%1#MobaFont%10%0%0%0%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%-1%_Std_Colors_0_%80%24%0%1%-1%<none>%%0#0# #-1"
+  content  = "terraform = #109#0%${digitalocean_droplet.web.ipv4_address}%22%root%%-1%-1%%%22%%0%0%0%_ProfileDir_\\Documents\\rsa\\terraform\\project_server%%-1%0%0%0%%1080%%0%0%1#MobaFont%10%0%0%0%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%-1%_Std_Colors_0_%80%24%0%1%-1%<none>%%0#0# #-1"
   filename = "${path.module}/terraform.moba"
 }
 
